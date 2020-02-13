16aa9cb25c75ffd9abd062ae8acc22eae45fc179
rezlers <rezlers@mail.ru>
2020-02-13 23:08:02 +0700

    Add some templates

--- Templates/Login.php
+++ Templates/Login.php
@@ -1,5 +1,5 @@
 <?php
-    use Kernel\Request as Request;
+    use Kernel\Request\RequestInterface as Request;
 ?>
 <!DOCTYPE html>
 <html lang="eng">
@@ -12,7 +12,8 @@
     <b>
         Этот текст будет полужирным, <i>а этот — ещё и курсивным</i>.
         <?php
-            echo $request->getParam('key');
+        /** @var Request $request */
+        echo $request->getParam('key');
         ?>
     </b>
 </p>
