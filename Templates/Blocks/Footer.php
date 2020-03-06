6b59d6a873c552b8d5203eb9ef9a57334709a10e
rezlers <rezlers@mail.ru>
2020-03-06 12:36:17 +0700

    footer layout

--- Templates/Blocks/Footer.php
+++ Templates/Blocks/Footer.php
@@ -1,13 +1,7 @@
 <?php
 ?>
 
-<div class="container">
-    <div class="navbar-fixed-bottom navbar-default">
-        <div class="container-fluid">
-            <footer>
-                <p class="lead"><small class="text-muted">Cool sign</small></p>
-            </footer>
-        </div>
-    </div>
-</div>
+<footer class="modal-footer">
+    <p class="lead"><small class="text-muted">Cool sign</small></p>
+</footer>
 
